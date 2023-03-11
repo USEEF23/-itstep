a80baf3989c8651ffefda79c976585c136fe76c2
ahme_x1qj <ahmadovyusif13@gmail.com>
2023-03-11 13:06:34 +0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lesson 3-GIT. OOP.py

--- lesson 3-GIT. OOP.py
+++ lesson 3-GIT. OOP.py
@@ -1,72 +1,66 @@
-class BMW:
-  price=11000
-  horsePower=25000
-  engine=2.0
-  petrol="gasoline"
-  breakTime=7
-  breakDistance=9
-  maxTemp=100
-class Toyota:
-  price=160000
-  horsePower=40000
-  engine=2.5
-  petrol="disel"
-  breakTime=20
-  breakDistance=27
-  maxTemp=200
-class Mercedes:
-  price=1500000
-  horsePower=60000
-  engine=3.7
-  petrol="premium gasoline"
-  breakTime=40
-  breakDistance=50
-  maxTemp=150
-class Chevrolet:
-  price=78000
-  horsePower=50000
-  engine=2.1
-  petrol="gasoline"
-  breakTime=10
-  breakDistance=13
-  maxTemp=100
-mat=input("Enter material: ")
-type=input("Enter car BMW/Mercedes/Toyota/Chevrolet: ")
-if type=="BMW":
-  print(mat,type)
-  print("price",BMW.price)
-  print("horse power",BMW.horsePower)
-  print("engine",BMW.engine)
-  print("petrol",BMW.petrol)
-  print("break time",BMW.breakTime)
-  print("break distance",BMW.breakDistance)
-  print("max temp",BMW.maxTemp)
-elif type=="Mercedes":
-  print(mat,type)
-  print("price",Mercedes.price)
-  print("horse power",Mercedes.horsePower)
-  print("engine",Mercedes.engine)
-  print("petrol",Mercedes.petrol)
-  print("break time",Mercedes.breakTime)
-  print("break distance",Mercedes.breakDistance)
-  print("max temp",Mercedes.maxTemp)
-elif type=="Toyota":
-  print(mat,type)
-  print("price",Toyota.price)
-  print("horse power",Toyota.horsePower)
-  print("engine",Toyota.engine)
-  print("petrol",Toyota.petrol)
-  print("break time",Toyota.breakTime)
-  print("break distance",Toyota.breakDistance)
-  print("max temp",Toyota.maxTemp)
-elif type=="Chevrolet":
-  print(mat,type)
-  print("price",Chevrolet.price)
-  print("horse power",Chevrolet.horsePower)
-  print("engine",Chevrolet.engine)
-  print("petrol",Chevrolet.petrol)
-  print("break time",Chevrolet.breakTime)
-  print("break distance",Chevrolet.breakDistance)
-  print("max temp",Chevrolet.maxTemp)
-else:
-  print("Please select from catalog")
+import random
+l=True
+
+class PetLife:
+    def __init__(self, name):
+        self.name = name
+        self.rest = 50
+        self.happiness = 10
+        self.training = 100
+        self.alive = True
+    def to_study(self):
+        print("Time to train")
+        self.happiness += 0.12
+        self.rest -= 5
+        self.training+=100
+    def to_sleep(self):
+        print("I will sleep")
+        self.rest += 3
+
+    def trainingLevel(self):
+        self.training+=10
+        return self.training
+
+    def to_chill(self):
+        print("Rest time")
+        self.rest += 5
+        self.happiness -= 0.1
+        self.training
+
+    def is_alive(self):
+        if self.rest < -0.5:
+            print("Cast out…")
+            self.alive = False
+            l=False
+        elif self.happiness <= 0:
+            print("Depression…")
+            self.alive = False
+            l=False
+        elif self.training > 400:
+            print("Passed externally…")
+            self.alive = False
+            l=False
+
+    def end_of_day(self):
+        print(f"Happines = {self.happiness}")
+        print(f"Progress ={round(self.training, 2)}")
+    def live(self, day):
+        day = "Day" + str(day) + "of" +self.name + "life"
+        print(f"{day:=^50}")
+        if self.training <100:
+            self.to_study()
+        live_cube = random.randint(1, 3)
+        if live_cube == 1:
+             self.to_study()
+        elif live_cube == 2:
+             self.to_sleep()
+        elif live_cube == 3:
+            self.to_chill()
+            self.end_of_day()
+            self.is_alive()
+
+nick = PetLife(name=input("Enter name: "))
+for day in range(365):
+    if nick.alive == False:
+        break
+    nick.live(day)
